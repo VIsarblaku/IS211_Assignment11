6ded91cd9bd6bfd40fb34ead60593770a8efeb9e
VIsarblaku <62189149+VIsarblaku@users.noreply.github.com>
2021-12-08 19:25:07 +0100

    Add files via upload

---
+++ week 11.py
@@ -0,0 +1,15 @@
+
+
+
+from ask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+      username = request.form['username']
+      return redirect(url_for('name', username=username))
+    return render_template('index.html', title='home')
+@app.route('/<username>', methods=['GET', 'POST'])
+def name(username):
+  return render_template('index.html', title='name', username=username)
